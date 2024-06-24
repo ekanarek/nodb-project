8384e1910ba922efb241279faedad232fd4df41b
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-24 10:29:10 -0400

    Remove boilerplate code

--- server/index.js
+++ server/index.js
@@ -2,10 +2,4 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.all('*', (req, res) => {
-    res.send(`<h1>This is the home page</h1>`)
-})
-
-app.get('/', (req, res) => res.send("Hello world!"))
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
