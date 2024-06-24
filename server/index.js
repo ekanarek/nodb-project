e087c245cccbbc623a367a8affce8ce8bafcff2c
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-24 10:28:18 -0400

    Initialize server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
-const express = require(`express`);
+import express from 'express';
 const app = express();
+const port = 3000;
 
-app.all('*', (req, res) => {})
+app.all('*', (req, res) => {
+    res.send(`<h1>This is the home page</h1>`)
+})
+
+app.get('/', (req, res) => res.send("Hello world!"))
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
