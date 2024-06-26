9b14a3b02c912526b3e433e55bce733cefce4d7b
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-26 10:19:04 -0400

    Started express server

--- server/index.js
+++ server/index.js
@@ -2,4 +2,4 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
