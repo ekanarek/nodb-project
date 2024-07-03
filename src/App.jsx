da0065b93df667fb29d19089a6638de97fcb238d
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-03 10:09:22 -0400

    Set up App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import GridContainer from './components/GridContainer/GridContainer.jsx';
+
+const TEST_DATA = [
+  { title: "Ghost Forest", author: "Pik-Shuen Fung", genre: "Fiction", rating: 8, coverImg: "https://m.media-amazon.com/images/I/71sz0d8eO+L._AC_UF1000,1000_QL80_.jpg", id: 0 },
+  { title: "Only If You're Lucky", author: "Stacy Willingham", genre: "Thriller", rating: 5, coverImg: "https://m.media-amazon.com/images/I/81lwDWtHxcL._AC_UF1000,1000_QL80_.jpg", id: 1 },
+  { title: "Words of Radiance", author: "Brandon Sanderson", genre: "Fantasy", rating: 9, coverImg: "https://m.media-amazon.com/images/I/91tECQfrZTL._AC_UF1000,1000_QL80_.jpg", id: 2 },
+  { title: "The Woods All Black", author: "Lee Mandelo", genre: "Horror", rating: 10, coverImg: "https://m.media-amazon.com/images/I/81cer8MPnsL._AC_UF1000,1000_QL80_.jpg", id: 3 }
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <GridContainer />;
 }
 
 export default App
