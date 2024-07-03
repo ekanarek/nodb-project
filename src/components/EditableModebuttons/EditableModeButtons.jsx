99907a55278457f6cfdffd5217cf11da7ffb0cad
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-03 17:18:09 -0400

    Create static buttons

---
+++ src/components/EditableModebuttons/EditableModeButtons.jsx
@@ -0,0 +1,16 @@
+import './EditableModeButtons.css';
+
+export default function EditableModeButtons({ editMode }) {
+  return editMode ? (
+    <button>Save</button>
+  ) : (
+    <div className="edit-buttons">
+      <button>
+        <span className="material-symbols-outlined">edit</span>
+      </button>
+      <button>
+        <span className="material-symbols-outlined">delete</span>
+      </button>
+    </div>
+  );
+}
